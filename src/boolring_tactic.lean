36f4837abf72bda509b6eb04de672a9c50e3e0e8
Edward Lee <e45lee@uwaterloo.ca>
2020-12-20 12:10:50 -0500

    meet/join <=> union/intersection.

--- src/boolring_tactic.lean
+++ src/boolring_tactic.lean
@@ -6,13 +6,23 @@ open tactic
 open tactic.interactive 
 open freealg
 
+---------------------------- Conversion Tactics: [finset α] to [boolean_algebra α] -----------------------
+lemma intersection_is_meet (α : Type) [fintype α][decidable_eq α] (X Y : finset α) : X ∩ Y = X ⊓ Y := rfl
+lemma union_is_join (α : Type) [fintype α][decidable_eq α] (X Y : finset α) : X ∪ Y = X ⊔ Y := rfl
 
-meta def set_to_ring_eqn : tactic unit := do
+meta def set_to_lattice_eqn : tactic unit := do
+  -- might be a better way to do this, but this seems to work.
+  `[try {repeat {rw union_is_join at *}},
+    try {repeat {rw intersection_is_meet at *}}]
+  
+
+meta def lattice_to_ring_eqn : tactic unit := do
 `[try {simp only
     [top_to_ring, bot_to_ring, symm_diff_to_ring, inf_to_ring, sup_to_ring, 
       diff_to_ring, compl_to_ring, le_to_ring] at *}]
 
 
+------------------------------ Normalization Tactics (in a free boolean algebra) -------------------------
 meta def ids_list : lean.parser (list name) := types.list_of ident
 meta def meta_build_vector : list pexpr -> pexpr
 | [] := ``(vector.nil)
@@ -46,7 +56,8 @@ meta def tactic.interactive.simplify_sets (sets : parse ids_list): tactic unit :
     vname <- get_unused_name `V,
     tactic.interactive.introduce_varmap_rewrite vname sets,
     vname_expr <- get_local vname,
-    set_to_ring_eqn,
+    set_to_lattice_eqn,
+    lattice_to_ring_eqn,
     simp none tt ([``(freealg.on_one %%vname_expr),
                    ``(freealg.on_plus %%vname_expr),
                    ``(freealg.on_times %%vname_expr),
@@ -81,5 +92,8 @@ lemma bar_finset [fintype α][decidable_eq α](X Y Z: finset α):
     simplify_sets [X,Y,Z],
   end
 
--- it isn't great to have to use ⊓/⊔ rather than ∩/∪ in these last two examples. 
--- can we massage this with the tactic? 
+lemma baz_finset [fintype α][decidable_eq α](X Y Z: finset α):
+  X ∩ Y ∩ Z = Z ∩ Y ∩ (X ∪ ⊥) := 
+  begin
+    simplify_sets [X,Y,Z],
+  end
