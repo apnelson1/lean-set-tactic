e7305b05e4caaaf2de24aea7cd6dd7423252f727
Edward Lee <e45lee@uwaterloo.ca>
2020-12-22 16:38:51 -0500

    Make clear the conversion between prop/boolean algebra in the question.

--- src/metaquestion.lean
+++ src/metaquestion.lean
@@ -1,14 +1,20 @@
 import tactic
 
+def f {T : Type} [boolean_algebra T] (X Y Z P Q W: T) : Prop :=
+  (X ⊔ (Y ⊔ Z)) ⊔ ((W ⊓ P ⊓ Q)ᶜ ⊔ (P ⊔ W ⊔ Q)) = ⊤
+
 lemma foo_alg {α : Type} (A: boolean_algebra α) (X Y Z P Q W: α): 
-  (X ⊔ (Y ⊔ Z)) ⊔ ((W ⊓ P ⊓ Q)ᶜ ⊔ (P ⊔ W ⊔ Q)) = ⊤ :=
+  f X Y Z P Q W :=
 begin
+  simp [f],
   sorry
+  -- we know this is true as in testing simplify_sets solves this.
 end
 
-lemma foo_prop  (X Y Z P Q W: Prop): 
-  (X ⊔ (Y ⊔ Z)) ⊔ ((W ⊓ P ⊓ Q)ᶜ ⊔ (P ⊔ W ⊔ Q)) :=
+lemma foo_prop (X Y Z P Q W: Prop): 
+  f X Y Z P Q W :=
 begin
+  simp [f],
   unfold_projs,
   tauto!
 end
